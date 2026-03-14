9be1ce9e262f5ae31382f844794b219ca6015d29
maximilianotech <kinebulnes@gmail.com>
2026-03-13 21:32:48 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Header from './components/Header';
 import Home from './pages/Home';
+import Footer from './components/footer'; // Importamos el componente de Copyright
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    /* Usamos flex-col y min-h-screen para que el Footer siempre esté al fondo */
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      
+      {/* Encabezado de la aplicación */}
       <Header />
-      <Home />
+
+      {/* Contenido principal que crece para empujar al footer */}
+      <main className="flex-grow">
+        <Home />
+      </main>
+
+      {/* Tu firma y Copyright */}
+      <Footer />
+      
     </div>
   );
 }
