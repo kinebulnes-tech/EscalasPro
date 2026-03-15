4e99e02199d7097026d74527f71ff7741fb893ee
maximilianotech <kinebulnes@gmail.com>
2026-03-15 13:06:26 -0300

    Update scalesData.ts

--- src/data/scalesData.ts
+++ src/data/scalesData.ts
@@ -144,7 +144,7 @@ export const scales: Scale[] = [
   
   // --- RIGOR CIENTÍFICO ---
   bibliografia: "Podsiadlo D, Richardson S. The timed 'Up & Go': a test of basic functional mobility for frail elderly persons. J Am Geriatr Soc. 1991 Feb;39(2):142-8.",
-  referenciaUrl: "https://pubmed.ncbi.nlm.nih.gov/1991915/",
+  referenciaUrl: "https://pubmed.ncbi.nlm.nih.gov/1991946/",
   evidenciaClinica: "El punto de corte de 13.5 segundos fue validado por Shumway-Cook (2000) como predictor de caídas con un 87% de sensibilidad en adultos mayores.",
   preguntas: [
     { 
